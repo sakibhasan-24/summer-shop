da7a811b435eadcc9c746e2ad384564bd7287f3c
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-04 16:38:56 +0600

    product show

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnPromoCode = document.getElementById("btn-promo-code");
 const couponBtn = document.getElementById("coupon-btn");
 const allItems = document.querySelectorAll(".all-items");
+const selectedItems = document.getElementById("items-selected");
 for (let i = 0; i < allItems.length; i++) {
   allItems[i].addEventListener("click", function () {
     // console.log(allItems.length);
@@ -8,5 +9,18 @@ for (let i = 0; i < allItems.length; i++) {
     const productName = document.getElementsByClassName("product-name");
     // console.log(price[i].innerText);
     console.log(price[i].innerText, productName[i].innerText);
+
+    const count = selectedItems.childElementCount;
+    console.log(count);
+    const p = document.createElement("p");
+
+    p.classList.add("font-bold", "text-gray-400");
+    p.innerHTML = `<p>
+    ${count + 1} ${productName[i].innerText}
+    </p>
+   
+    
+    `;
+    selectedItems.appendChild(p);
   });
 }
