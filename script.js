3c4d9c59c5c86d09c0f9d545491c37f16a350ac8
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-04 16:30:06 +0600

    get value from card

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const btnPromoCode = document.getElementById("btn-promo-code");
 const couponBtn = document.getElementById("coupon-btn");
-const allItmes = document.getElementsByClassName("all-itmes");
-// console.log();
-const itmes = allItmes[0].children[0];
-console.log(items);
+const allItems = document.querySelectorAll(".all-items");
+for (let i = 0; i < allItems.length; i++) {
+  allItems[i].addEventListener("click", function () {
+    // console.log(allItems.length);
+    const price = document.getElementsByClassName("price");
+    const productName = document.getElementsByClassName("product-name");
+    // console.log(price[i].innerText);
+    console.log(price[i].innerText, productName[i].innerText);
+  });
+}
