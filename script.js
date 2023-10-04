fa8efabebcf09cd49b3089c33b9929758354a96b
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-04 15:15:19 +0600

    layout finish

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const btnPromoCode = document.getElementById("btn-promo-code");
+const couponBtn = document.getElementById("coupon-btn");
+const allItmes = document.getElementsByClassName("all-itmes");
+// console.log();
+const itmes = allItmes[0].children[0];
+console.log(items);
